956ea4416802492d46f4b08b2f94644510e49a0f
In-Hyeon <worlf0987@gmail.com>
2026-06-01 13:53:37 +0900

    fix: update server HOST

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,12 @@ import app from './app';
 import connectDB from './config/db';
 
 const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
 
 // 서버를 띄우기 전에 먼저 DB를 연결.
 connectDB().then(() => {
     // app.listen을 호출하는 순간, 서버가 지정된 포트에서 요청을 기다리기 시작함.
-    app.listen(PORT, () => {
+    app.listen(Number(PORT), HOST, () => {
         console.log(`✅ Server is running on http://localhost:${PORT}`);
     });
 });
